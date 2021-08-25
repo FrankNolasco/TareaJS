92c75da8224fbe25a50ab26286f28546caf3a5db
Frank Nolasco Velasquez <49081234+FranksitoKawai@users.noreply.github.com>
2021-08-25 09:48:52 -0500

    update list products

--- static/functions/jsons.js
+++ static/functions/jsons.js
@@ -6,28 +6,31 @@ const ProductosJSON = [
         price: "300"
     },
     {  
-        name : "PATIÑO",
-        img: "https://comercializadoradelnorte.com.mx/8245-thickbox_default/llanta-delinte-205-65-r15.jpg",
+        name : `Camara llanta 10''`,
+        img: "https://www.ferreteriafara.com/producto/camara-para-llanta-de-10",
         color: "#fff",
         price: "300"
     },
-    {  name : "LLanta",
-    img: "https://comercializadoradelnorte.com.mx/8245-thickbox_default/llanta-delinte-205-65-r15.jpg",
+    {   name : "Timon para bicicleta",
+        img: "https://bikemotorcortez.com/timon-renthal-cross-lazer",
         color: "#fff",
         price: "300"
     },
-    {  name : "LLanta",
-    img: "https://comercializadoradelnorte.com.mx/8245-thickbox_default/llanta-delinte-205-65-r15.jpg",
+    {  
+        name : "Cubre asientos para moto.",
+        img: "https://articulo.mercadolibre.com.mx/MLM-627838555-cubre-asiento-para-motos-_JM",
         color: "#fff",
         price: "300"
     },
-    {  name : "LLanta",
-    img: "https://comercializadoradelnorte.com.mx/8245-thickbox_default/llanta-delinte-205-65-r15.jpg",
+    {  
+        name : "Moto Rouser 180",
+        img: "https://www.blogdecoches.net/motos/rouser-180-2017-en-argentina.html",
         color: "#fff",
         price: "300"
     },
-    {  name : "LLanta",
-    img: "https://comercializadoradelnorte.com.mx/8245-thickbox_default/llanta-delinte-205-65-r15.jpg",
+    {  
+        name : "Retrovisor moto",
+        img: "https://www.motoblouz.es/espejo-retrovisor-chaft-safety-izquierdo-121890-v.html",
         color: "#fff",
         price: "300"
     },
